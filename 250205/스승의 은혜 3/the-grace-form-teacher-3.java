1605ff146f47e98cac4c4821e8b8210aad37324d
TaekyuKo <taekueko714@hufs.ac.kr>
2025-02-05 00:00:31 +0900

    Title: 스승의 은혜 3, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250205/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] p = new int[n];
+        int[] s = new int[n];
+        int[] sum = new int[n];
+        for(int i = 0;i<n;i++){
+            p[i] = sc.nextInt();
+            s[i] = sc.nextInt();
+            sum[i] = p[i]+s[i];
+        }
+        
+        int res = 0;
+        for(int i = 0;i<n;i++){
+            int[] tmp = new int[n];
+            for(int j = 0; j < n; j++){
+                tmp[j] = p[j] + s[j];
+            }
+            tmp[i] = p[i] / 2 + s[i];
+
+            Arrays.sort(tmp);
+
+            int student = 0;
+            int cnt = 0;
+        
+            for(int j = 0; j < n; j++) {
+                if(cnt + tmp[j] > b)
+                    break;
+                cnt += tmp[j];
+                student++;
+            }
+
+            res = Math.max(res, student);
+        }
+
+        System.out.println(res);
+
+    }
+}
