afd986baffabde1f02fb74e80177eaa4b4a8b1c1
고태규 <taekueko714@hufs.ac.kr>
2025-02-05 23:44:14 +0900

    Title: 빙산의 일각 2, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 250205/빙산의 일각 2/the-tip-of-the-iceberg-2.java
+++ 250205/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -4,24 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] h = new int[n];
+        int[] h = new int[n+1];
         int max = 0;
         for(int i = 0;i<n;i++){
             h[i] = sc.nextInt();
             max = Math.max(max,h[i]);
         }
+        h[n] = 0;
 
         int res = 0;
         for(int i = 0;i<=max;i++){
             int temp = 0;
-            for(int j = 0;j<n-1;j++){
+            for(int j = 0;j<n;j++){
                 if(h[j]-i>0 && h[j+1]-i<=0){
                     temp++;
                 }
             }
-            if(h[n-1]>0){
-                temp++;
-            }
+
             res = Math.max(res,temp);
         }
 
