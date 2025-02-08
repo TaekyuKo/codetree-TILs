f0d3a4f489f90e5189de178df69dd9e8147c3bb7
고태규 <taekueko714@hufs.ac.kr>
2025-02-08 20:45:39 +0900

    Title: 야바위, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250208/야바위/ya-rock.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+        for(int i = 0;i<n;i++){
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+            c[i] = sc.nextInt();
+        }
+
+        int res = 0;
+        int max_cnt = 0;
+        for(int i =1;i<=3;i++){
+            int dol = i;
+            int temp_cnt = 0;
+            for(int j = 0;j<n;j++){
+                if(a[j]==dol){
+                    dol = b[j];
+                }else if(b[j]==dol){
+                    dol = a[j];
+                }else{
+                    //
+                }
+
+                if(c[j]==dol){
+                    temp_cnt++;
+                }
+            }
+
+            if(max_cnt<temp_cnt){
+                max_cnt = temp_cnt;
+                res = i;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
