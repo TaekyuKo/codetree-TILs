0e09c0bf0f6b4a5db18fbb162a87edb0fdf07a92
고태규 <taekueko714@hufs.ac.kr>
2025-02-22 22:33:19 +0900

    Title: 원소 값들의 최대 합, Time: 165ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250222/원소 값들의 최대 합/maximum-sum-of-element-values.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] num = new int[n+1];
+        for(int i = 1;i<=n;i++){
+            num[i] = sc.nextInt();
+        }
+
+        int res_max = 0;
+        for(int i =1;i<=5;i++){
+            int temp = 0;
+            int point = i;
+            for(int j =0;j<m;j++){
+                temp+=num[point];
+                point = num[point];
+            }
+            res_max = Math.max(res_max,temp);
+        }
+
+        System.out.println(res_max);
+        
+    }
+}
